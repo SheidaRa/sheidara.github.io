1d688367638e8b87a1d79249eb4aafbc004badd6
Sheida Rashidi <84582226+SheidaRa@users.noreply.github.com>
2024-05-20 19:58:31 +0200

    Updated Dev Cards styling

--- src/Components/PortfolioCard.js
+++ src/Components/PortfolioCard.js
@@ -1,32 +1,41 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-const PortfolioCard = ({ project, tools, link, image = 'images/placeholder.jpg' }) => {
+const PortfolioCard = ({
+  project,
+  tools,
+  link,
+  image = "images/placeholder.jpg",
+}) => {
+  const divStyle = {
+    backgroundImage: `url(${image})`,
+  };
 
-    const divStyle = {
-        backgroundImage: `url(${image})`
-    }
-
-    return (
-        <div className='portfolio-card'>
-            <div className='portfolio-img' style={divStyle}>
-                <div className='portfolio-content'>
-                    <h3>{project}</h3>
-                    <div className='tools'>
-                        {tools.map((tool, index) =>
-                            <span key={index}>
-                                <p>{tool}</p>
-                            </span>
-                        )}
-                    </div>
-                    <a className='button' href={link} target='blank'>
-                        <span>Get code</span><FontAwesomeIcon icon={faGithub} />
-                    </a>
-                </div>
-            </div>
+  return (
+    <>
+      <div className="portfolio-card dev-card">
+        <div className="portfolio-img" style={divStyle}>
+          <div className="portfolio-content">
+            <a className="button" href={link} target="blank">
+              <span>Get code</span>
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </div>
         </div>
-    )
-}
+      </div>
+      <div>
+        <h3>{project}</h3>
+        <div className="tools">
+          {tools.map((tool, index) => (
+            <span key={index}>
+              <p>{tool}</p>
+            </span>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default PortfolioCard
+export default PortfolioCard;

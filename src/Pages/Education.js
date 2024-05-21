652b5a62050a55910ee6a6c8c5de53205974c119
Sheida Rashidi <84582226+SheidaRa@users.noreply.github.com>
2024-05-21 12:47:50 +0200

    Fixed typos

--- src/Pages/Education.js
+++ src/Pages/Education.js
@@ -8,7 +8,7 @@ const Education = () => {
         <EducationCard
           school={"Macalester College"}
           place={"United States of America"}
-          degree={"Bachelors in Computer Scirnce with a Minor in Psychology"}
+          degree={"Bachelors in Computer Science with a Minor in Psychology"}
           image={"images/schools/Mac.jpg"}
         />
       </div>
@@ -17,14 +17,14 @@ const Education = () => {
         <EducationCard
           school={"University of Bradford"}
           place={"United Kingdom"}
-          degree={"Study Abroud Program"}
+          degree={"Study Abroad Program"}
           image={"images/schools/bradford.jpg"}
         />
       </div>
 
       <div className="col-lg-4 offset-lg-2 col-10 offset-xsm-1">
         <EducationCard
-          school={"Li Po Chun Unted World College of Hong Kong"}
+          school={"Li Po Chun United World College of Hong Kong"}
           place={"Hong Kong"}
           degree={"International Baccalaureate Diploma"}
           image={"images/schools/LPC.jpeg"}

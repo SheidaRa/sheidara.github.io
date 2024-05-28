d086363417ce847dd7c5c4d97f71511e573d2ac7
Sheida Rashidi <84582226+SheidaRa@users.noreply.github.com>
2024-05-28 22:30:37 +0200

    Pop up window header styling

--- src/Components/InfoModal.js
+++ src/Components/InfoModal.js
@@ -6,25 +6,33 @@ const InfoModal = forwardRef(
     return (
       <div
         ref={ref}
-        className={`infoModal ${expandedItem === 0 ? "hideModal" : "showModal"
-          }`}
+        className={`infoModal ${
+          expandedItem === 0 ? "hideModal" : "showModal"
+        }`}
       >
         <div className="cardContentHeader">
-          {title === "GogyUp" && <a className="button" href={codeLink} target="blank">
-            Canva File
-          </a>}
-          {(title === "Exploring Emotions in VR" || title === "TouchChat") && <a className="button" href={codeLink} target="blank">
-            Medium Article
-          </a>}
-          {(title !== "Exploring Emotions in VR" && title !== "TouchChat" && title !== "GogyUp") && <a className="button" href={codeLink} target="blank">
-            Figma File
-          </a>}
+          {title === "GogyUp" && (
+            <a className="button" href={codeLink} target="blank">
+              Canva File
+            </a>
+          )}
+          {(title === "Exploring Emotions in VR" || title === "TouchChat") && (
+            <a className="button" href={codeLink} target="blank">
+              Medium Article
+            </a>
+          )}
+          {title !== "Exploring Emotions in VR" &&
+            title !== "TouchChat" &&
+            title !== "GogyUp" && (
+              <a className="button" href={codeLink} target="blank">
+                Figma File
+              </a>
+            )}
           <RxCross2 onClick={() => setExpandedItem(0)} className="closeIcon" />
         </div>
-        <h2>{title}</h2>
-        {content}
-        <div className="closeIcon-div">
-          <RxCross2 onClick={() => setExpandedItem(0)} className="closeIcon" />
+        <div className="infoModal-content">
+          <h2>{title}</h2>
+          {content}
         </div>
       </div>
     );

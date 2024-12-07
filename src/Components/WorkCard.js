dd047f69f9bbcdef4d9839ea8df96c70cd0bced9
Sheida Rashidi <84582226+SheidaRa@users.noreply.github.com>
2024-12-07 18:19:19 +0100

    Update WorkCard.js

--- src/Components/WorkCard.js
+++ src/Components/WorkCard.js
@@ -6,16 +6,26 @@ const WorkCard = ({
   date,
   image = "images/placeholder.jpg",
   handleToggleExpansion,
+  hovername = " Involvement details ",
 }) => {
+  const divStyle = {
+    backgroundImage: `url(${image})`,
+  };
   return (
-    <div className="work-card">
-      <div className="work-img" onClick={handleToggleExpansion}>
-        <img src={image} />
+    <>
+      <div className="portfolio-card" onClick={handleToggleExpansion}>
+        <div className="portfolio-img" style={divStyle}>
+          <div className="portfolio-content">
+            <span className="design"> {hovername} </span>
+          </div>
+        </div>
       </div>
-      <h2 className="custom-card-title">{experience}</h2>
-      <h4>{place}</h4>
-      <h4>{date}</h4>
-    </div>
+      <div className="work-card">
+        <h2 className="custom-card-title">{experience}</h2>
+        <h4>{place}</h4>
+        <h4>{date}</h4>
+      </div>
+    </>
   );
 };
 
